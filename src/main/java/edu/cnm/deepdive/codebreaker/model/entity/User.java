b1d85d0ee8a9841e398b824079d568651074309d
Nick Bennett <nick@nickbenn.com>
2022-02-23 15:22:34 -0700

    Starting data model.

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -0,0 +1,122 @@
+package edu.cnm.deepdive.codebreaker.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    name = "user_profile"
+)
+public class User {
+
+  @NonNull
+  @Id
+  @GeneratedValue
+  @Column(name = "user_profile_id", updatable = false, nullable = false, columnDefinition = "UUID")
+  @JsonIgnore
+  private UUID id;
+
+  @NonNull
+  @Column(updatable = false, nullable = false, unique = true, columnDefinition = "UUID")
+  @JsonProperty(value = "id", access = Access.READ_ONLY)
+  private UUID externalKey;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(updatable = false, nullable = false)
+  @JsonProperty(access = Access.READ_ONLY)
+  private Date created;
+
+  @NonNull
+  @Column(updatable = false, nullable = false, unique = true, length = 30)
+  @JsonIgnore
+  private String oauthKey;
+
+  @NonNull
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  @JsonIgnore
+  private Date connected;
+
+  @NonNull
+  @Column(nullable = false, unique = true)
+  private String displayName;
+
+  @Column(length = 255)
+  private String avatar;
+
+  @Column(nullable = false)
+  private boolean incognito;
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public UUID getExternalKey() {
+    return externalKey;
+  }
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  @NonNull
+  public String getOauthKey() {
+    return oauthKey;
+  }
+
+  public void setOauthKey(@NonNull String oauthKey) {
+    this.oauthKey = oauthKey;
+  }
+
+  @NonNull
+  public Date getConnected() {
+    return connected;
+  }
+
+  public void setConnected(@NonNull Date connected) {
+    this.connected = connected;
+  }
+
+  @NonNull
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(@NonNull String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getAvatar() {
+    return avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = avatar;
+  }
+
+  public boolean isIncognito() {
+    return incognito;
+  }
+
+  public void setIncognito(boolean incognito) {
+    this.incognito = incognito;
+  }
+}
